37d5d1c7e70c196b7553910c65a9e9f77f48a8fb
KooAme <dg.changeworld@gmail.com>
2023-03-14 09:32:56 +0900

    🐘Rename: config -> app.module file

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,22 +2,34 @@ import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppService } from './app.service';
-import { typeORMConfig } from './database/typeorm.config';
+// import { typeORMConfig } from './database/typeorm.config';
 import { LoggerMiddleware } from './middlewares/logger.middlewares';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { DiarysController } from './diary/diary.controller';
 import { DiarysModule } from './diary/diary.module';
+import { Diary } from './diary/entities/diary.entity';
+import { User } from './users/entities/user.entity';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
     }),
-    TypeOrmModule.forRoot(typeORMConfig),
     AuthModule,
     UsersModule,
     DiarysModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      entities: [User, Diary],
+      synchronize: true,
+      // logging: true,
+    }),
   ],
   controllers: [],
   providers: [],
