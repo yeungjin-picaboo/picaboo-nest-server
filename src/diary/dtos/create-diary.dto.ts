11f7b9109d4f55823faaa54342552064ba65af4e
inhyoe <melon7256@naver.com>
2023-03-15 20:36:08 +0900

    [ud] asd

--- src/diary/dtos/create-diary.dto.ts
+++ src/diary/dtos/create-diary.dto.ts
@@ -1,12 +1,10 @@
 import { CoreOutput } from 'src/common/dto/output.dto';
-import { Entity } from 'typeorm';
+
 import { PickType } from '@nestjs/mapped-types';
 import { Diary } from '../entities/diary.entity';
 
-@Entity()
 export class CreateDiaryDto extends PickType(Diary, ['title', 'content']) {} // 일기 생성 DB
 
-@Entity()
 export class CreateMoodDto extends PickType(Diary, ['emotion']) {}
 
 export class CreateDiaryOutput extends CoreOutput {}
