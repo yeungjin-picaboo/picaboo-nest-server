1345abae6b908e7f42c255ed3492e06c92c53c90
ingyoe <melon7256@naver.com>
2023-03-16 09:00:01 +0900

    [feat] Add picture making

--- src/picture/picture.module.ts
+++ src/picture/picture.module.ts
@@ -2,10 +2,10 @@ import { Module } from '@nestjs/common';
 import { PictureController } from './picture.controller';
 import { PictureService } from './picture.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { create_picture } from './entities/create_picture.entity';
+import { createPicture } from './entities/create_picture.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([create_picture])],
+  imports: [TypeOrmModule.forFeature([createPicture])],
   controllers: [PictureController],
   providers: [PictureService]
 })
--- src/picture/picture.service.ts
+++ src/picture/picture.service.ts
@@ -1,28 +1,27 @@
 import { Injectable } from '@nestjs/common';
-import { create_picture } from './entities/create_picture.entity';
+import { createPicture } from './entities/create_picture.entity';
 import { DataSource, Repository } from 'typeorm';
 import { CreatePictureDto } from './dto/create_picture.dto';
-import { exec } from 'child_process';
+import { ChildProcess, exec, execSync } from 'child_process';
 import * as fs from 'fs';
 import { InjectRepository } from '@nestjs/typeorm';
 
 @Injectable()
-export class PictureService extends Repository<create_picture> {
-  constructor(@InjectRepository(create_picture) private dataSource: DataSource) {
-    super(create_picture, dataSource.manager);
+export class PictureService extends Repository<createPicture> {
+  constructor(@InjectRepository(createPicture) private dataSource: DataSource) {
+    super(createPicture, dataSource.manager);
   }
   async createPicture(createPictureDto: CreatePictureDto): Promise<String> {
-    // : Promise<create_picture>
     const { title, user_email, user_name } = createPictureDto;
-
-    exec(`./test.sh "${user_name}" "${title}"`);
-    // After excecute this fucnction,
+    
+     exec(`./src/picture/create_picture.sh "${user_name}" "${title}"`);
+    //  After excecute this fucnction,
 
     const lastDir = fs.readdirSync(`${process.env.PUBLIC_LINK}${user_name}`, 'utf-8');
     const lastFile = lastDir[lastDir.length - 1];
 
     const picture_url = `${process.env.PUBLIC_LINK}${user_name}` + lastFile;
-
+   //  console.log('a = ', a.toString('utf-8'));
     console.log(__dirname + `${user_name}`);
     console.log(process.env.DB_USERNAME);
     const create_picture = this.create({
