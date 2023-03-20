06b1e471f74f78c89e5082ac5ca272c3a678d984
KooAme <dg.changeworld@gmail.com>
2023-03-20 22:45:39 +0900

    ✨Feat: connect CORS

--- src/main.ts
+++ src/main.ts
@@ -17,6 +17,7 @@ async function bootstrap() {
 
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('swagger', app, document);
+  app.enableCors();
 
   app.useGlobalPipes(
     new ValidationPipe({
