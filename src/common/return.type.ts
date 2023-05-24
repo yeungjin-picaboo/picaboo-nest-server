70a4956c261f5b0d61f9fc30beab2f101a61cd9c
inhyoe <melon7256@naver.com>
2023-05-24 23:58:22 +0900

    [feat] add return type

--- src/common/return.type.ts
+++ src/common/return.type.ts
@@ -1,8 +1,14 @@
-function returnMsg(ok: boolean, message: string) {
-  return {
+function returnMsg(ok: boolean, message: string, error?: Error) {
+  const result = {
     ok,
     message
   };
+
+  if (error) {
+    result['errorMsg'] = error;
+  }
+
+  return result;
 }
 
 export { returnMsg };
